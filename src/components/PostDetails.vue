d8ec495043136845f0fd8111a090f2532e65bfa1
saikiranmaddipati <maddipatisaikiran24@gmail.com>
2022-09-30 11:29:36 +0530

    blog-page

---
+++ src/components/PostDetails.vue
@@ -0,0 +1,71 @@
+<template>
+    <div>
+        <h1 class="text-center">post details</h1>
+        <div v-for="post in postDetails" :key="post.id">
+            <q-card
+      class="my-card text-white"
+      style="background: radial-gradient(circle, #35a2ff 0%, #014a88 100%)"
+    >
+    <q-card-section>
+        <h1 class="heading">{{post.id}}. {{post.title}}</h1>
+      </q-card-section>
+    <q-card-section class="q-pt-none">
+        <p class="paragraph">{{post.body}}</p>
+    </q-card-section>
+    </q-card>
+        </div>
+    </div>
+</template>
+
+<script>
+import Axios from 'axios'
+export default {
+  name: 'PostDetails',
+  data () {
+    return {
+      postDetails: []
+    }
+  },
+  mounted () {
+    this.getPostDetails()
+  },
+  methods: {
+    getPostDetails () {
+      Axios.get('https://jsonplaceholder.typicode.com/posts').then((response) => {
+        this.postDetails = response.data
+      })
+        .catch(function (error) {
+          console.log(error)
+        })
+      Axios.post('https://jsonplaceholder.typicode.com/posts', {
+        firstName: 'saikiran',
+        lastName: 'maddipati'
+      })
+        .then(function (response) {
+          console.log(response)
+        })
+        .catch(function (error) {
+          console.log(error)
+        })
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+.my-card
+  width: 100%
+  max-width: 1100px
+  margin-bottom: 25px
+.heading
+    font-family: "Roboto"
+    font-size: 30px
+    font-weight: bold
+    text-align: left
+    margin-bottom: 30px
+.paragraph
+    font-family: "Roboto"
+    font-size: 25px
+    color: lightblue
+    text-align: left
+</style>
